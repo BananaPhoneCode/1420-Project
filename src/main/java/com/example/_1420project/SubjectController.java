c3ecd88f2af8cc1db0a941cd606e59598ce082ac
Jthedog22 <jeremyartun@gmail.com>
2025-03-04 16:05:17 -0500

    -jog

--- src/main/java/com/example/_1420project/SubjectController.java
+++ src/main/java/com/example/_1420project/SubjectController.java
@@ -1,4 +1,31 @@
 package com.example._1420project;
 
-public class SubjectController {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SubjectController implements Initializable{
+    @FXML
+    private ListView<String> myListView;
+    @FXML
+    private Label myLabel;
+    String[] subject = {"mechanics", "calc", "french"};
+    String currentSubject;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        myListView.getItems().addAll(subject);
+        myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                currentSubject = myListView.getSelectionModel().getSelectedItem();
+                myLabel.setText(currentSubject);
+            }
+        });
+    }
 }
