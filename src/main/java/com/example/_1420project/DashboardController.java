683bca93e951cddbc0a0d08fceba802041712ddc
AishaZAhmed <aishazahmed22@gmail.cm>
2025-04-03 14:30:08 -0400

    tooba's stuff commit

--- src/main/java/com/example/_1420project/DashboardController.java
+++ src/main/java/com/example/_1420project/DashboardController.java
@@ -101,7 +101,7 @@ public class DashboardController implements Initializable {
     //Menu Buttons
     public void dashboard(ActionEvent event) throws IOException {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("DashboardAdmin.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
             Parent root = loader.load();
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             Scene scene = new Scene(root);
